d318e7967bd9f465e13232c6be0ca31846ec2ef7
Mitko Lyungov <mitko.lyungov@gmail.com>
2022-07-02 09:51:36 +0300

    added a new folder and new files

---
+++ JAVA_ALGORITHMS-FUNDAMENTALS/Graph Theory, Traversal and Shortest Paths/Main.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        List<List<Integer>> graph = new ArrayList<> ();
+
+        int n = Integer.parseInt (scanner.nextLine ());
+
+        for (int i = 0; i < n; i++) {
+            String nextLine = scanner.nextLine ();
+            if (nextLine.trim ().equals ("")) {
+                graph.add (new ArrayList<> ());
+                continue;
+            }
+
+            graph.add (Arrays.stream (nextLine.split ("\\s+")).map (Integer::parseInt).collect (Collectors.toList ()));
+        }
+
+        List<Deque<Integer>> connectedComponents = getConnectedComponents (graph);
+
+        for (Deque<Integer> connectedComponent : connectedComponents) {
+            System.out.print ("Connected component: ");
+            for (Integer integer : connectedComponent) {
+                System.out.print (integer + " ");
+            }
+            System.out.println ();
+        }
+    }
+
+    public static List<Deque<Integer>> getConnectedComponents(List<List<Integer>> graph) {
+        boolean[] visited = new boolean[graph.size ()];
+        List<Deque<Integer>> components = new ArrayList<> ();
+
+        for (int start = 0; start < graph.size (); start++) {
+            if (!visited[start]) {
+                components.add (new ArrayDeque<> ());
+                dfs (start, components, graph, visited);
+            }
+        }
+
+        return components;
+    }
+
+    private static void dfs(int node, List<Deque<Integer>> components, List<List<Integer>> graph, boolean[] visited) {
+        if (!visited[node]) {
+            visited[node] = true;
+            for (int child : graph.get (node)) {
+                dfs (child, components, graph, visited);
+            }
+            components.get (components.size () - 1).offer (node);
+        }
+    }
+
+    public static Collection<String> topSort(Map<String, List<String>> graph) {
+        return null;
+    }
+}
